a48f9e6dab059819803fd2ec871687cd496d9a39
Shahnawaz <49604292+shahnawazm786@users.noreply.github.com>
2021-12-28 08:37:47 +0530

    Commit the program

---
+++ src/main/java/interfaces/DemoInterface.java
@@ -0,0 +1,20 @@
+package interfaces;
+
+public class DemoInterface implements IExample{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void click() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void sendText() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
