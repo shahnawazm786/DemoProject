b4d9fa516a24bdb76abe54a7de51741dadc5e9e3
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-01-07 22:10:08 +0530

    Throw and Throws

--- src/main/java/ExceptionHandling/ExceptionClassDemo1.java
+++ src/main/java/ExceptionHandling/ExceptionClassDemo1.java
@@ -9,11 +9,21 @@ public class ExceptionClassDemo1 {
 			ie.printStackTrace();
 		}
 	}
+
+	public static void NumberDivideByZeroException() {
+		int a=100,b=0,c;
+		try{
+		c=a/b;}catch(ArithmeticException ae) {
+			ae.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Try catch example");
 		Waits();
 		System.out.println("After 3 seconds - message displayed");
+		NumberDivideByZeroException();
 	}
-
+	
 }
