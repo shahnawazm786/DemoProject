6c368c3bab42a3f953375bd018eb8130d3b050cd
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-01-08 09:36:01 +0530

    exception 08 jan 2021

---
+++ src/main/java/ExceptionHandling/ExceptionsDemo.java
@@ -0,0 +1,23 @@
+package ExceptionHandling;
+
+public class ExceptionsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java");
+		System.out.println(100+200);
+		System.out.println(100>50);
+		System.out.println(200<60);
+		System.out.println(3000/100);
+		System.out.println(2000%7);
+		System.out.println("Line 13");
+		System.out.println("Line 14");
+		System.out.println("Line 15");
+		String name=null;
+		System.out.println(name.length());
+		System.out.println("Line 18");
+		System.out.println("Line 19");
+
+	}
+
+}
