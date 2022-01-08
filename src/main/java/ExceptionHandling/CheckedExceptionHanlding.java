de9f0becdb59a5a759cdc988cd28b220ac82a21d
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-01-08 09:51:28 +0530

    checked1

--- src/main/java/ExceptionHandling/CheckedExceptionHanlding.java
+++ src/main/java/ExceptionHandling/CheckedExceptionHanlding.java
@@ -4,24 +4,31 @@ import java.io.*;
 public class CheckedExceptionHanlding {
 
 	public static void chekcedException() throws IOException{
+		System.out.println("Writing content into the file");
 		BufferedWriter bw=new BufferedWriter(new FileWriter("src/test/resources/chekced.txt"));
 		bw.write("Hello checked exception");
 		bw.close();
+		System.out.println("File created and written");
 	}
 
 	public static void tryCatchException() {
 		try {
+		System.out.println("Writing content into the file");
 		BufferedWriter bw=new BufferedWriter(new FileWriter("src/test/resources/chekced.txt"));
-		bw.write("Hello checked exception");
+		bw.write("Hello checked exception, second time");
 		bw.close();
+		System.out.println("File created and written");
 		}catch(IOException ex) {
 			ex.printStackTrace();
 		}
 	}
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) 
+	{
+	//throws IOException {
 		// TODO Auto-generated method stub
-		chekcedException();
+		//chekcedException();
+		tryCatchException();
 	}
 
 }
