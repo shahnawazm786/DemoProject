242f6e2c34dbf76239cea27cf7bedd0bb6e851b7
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-01-15 22:44:43 +0530

    List interface

---
+++ src/main/java/collections/ListInterface.java
@@ -0,0 +1,14 @@
+package collections;
+import java.util.*;
+public class ListInterface {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List aList=new ArrayList();
+		List linkedList=new LinkedList();
+		List vList=new Vector();
+		List lStatck=new Stack();
+		
+	}
+
+}
