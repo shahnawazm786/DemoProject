0798ff6032ed77beb96bf1e0e4b567d618ea9fa3
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-01-16 22:37:01 +0530

    wait explicit

--- src/test/java/waits/WaitsExample.java
+++ src/test/java/waits/WaitsExample.java
@@ -10,23 +10,34 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.concurrent.TimeoutException;
+
 public class WaitsExample {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws TimeoutException{
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "src/test/resources/Drivers/chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.manage().timeouts().pageLoadTimeout(50,TimeUnit.SECONDS);
+		//driver.manage().timeouts().pageLoadTimeout(50,TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(50));
+		
 		driver.get("https://askomdch.com/");
+		By accountLink=By.xpath("//a[text()='Account']");
 		By username=By.id("reg_username");
+		click(driver,accountLink,20,5);
 		sendKeys(driver,username,20,5,"test");
 	}
 	public static void sendKeys(WebDriver driver,By locator,long timeout,long polling,String data){
-		WebDriverWait wait=new WebDriverWait(driver,timeout);
+		WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(timeout));
 		wait.pollingEvery(Duration.ofSeconds(polling))
 		.until(ExpectedConditions.visibilityOfElementLocated(locator)).sendKeys(data);
 		
+	}
+	public static void click(WebDriver driver,By locator,long timeout,long polling) {
+		WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(timeout));
+		wait.pollingEvery(Duration.ofSeconds(polling))
+		.until(ExpectedConditions.visibilityOfElementLocated(locator)).click();	
 	}
 
 }
