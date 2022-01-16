3d018a31170307b7b6e6f5c5fe8228530d963824
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-01-16 22:27:43 +0530

    wait

--- src/test/java/waits/WaitsExample.java
+++ src/test/java/waits/WaitsExample.java
@@ -1,10 +1,14 @@
 package waits;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.ElementNotSelectableException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WaitsExample {
 
@@ -15,7 +19,14 @@ public class WaitsExample {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().pageLoadTimeout(50,TimeUnit.SECONDS);
 		driver.get("https://askomdch.com/");
-		By username=By.id("");
+		By username=By.id("reg_username");
+		sendKeys(driver,username,20,5,"test");
+	}
+	public static void sendKeys(WebDriver driver,By locator,long timeout,long polling,String data){
+		WebDriverWait wait=new WebDriverWait(driver,timeout);
+		wait.pollingEvery(Duration.ofSeconds(polling))
+		.until(ExpectedConditions.visibilityOfElementLocated(locator)).sendKeys(data);
+		
 	}
 
 }
