bcac512cb51caa8494455b249c05c8bf1a827c29
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-01-22 10:05:44 +0530

    hashMapEnum

--- src/main/java/map/EnumInHashTableExample.java
+++ src/main/java/map/EnumInHashTableExample.java
@@ -6,6 +6,8 @@ public class EnumInHashTableExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		enumHashTable();
+		System.out.println("Out put of Hash Map using Enum");
+		enumHashMap();
 	}
 	public static void enumHashTable() {
 		Hashtable<Color,String> enumColor=new Hashtable<Color,String>();
@@ -17,6 +19,16 @@ public class EnumInHashTableExample {
 			System.out.println("Keys -> "+c.getKey() +"  Value -> "+c.getValue());
 		}
 		
+	}
+	public static void enumHashMap() {
+		HashMap<Color,String> enumColor=new HashMap<Color,String>();
+		enumColor.put(Color.RED, "Red Color");
+		enumColor.put(Color.GREEN, "Green Color");
+		enumColor.put(Color.YELLOW, "Yellow Color");
+		enumColor.put(Color.BLUE, "Blue Color");
+		for(Map.Entry<Color, String> c:enumColor.entrySet()) {
+			System.out.println("Keys -> "+c.getKey() +"  Value -> "+c.getValue());
+		}
 	}
 
 }
