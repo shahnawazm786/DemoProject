54583aa7906df6f37ec8774ee38e202c09da9f12
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-01-22 22:47:48 +0530

    list map

---
+++ src/main/java/map/HashMapAndListExample.java
@@ -0,0 +1,33 @@
+package map;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HashMapAndListExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	public static void hashMapAndList() {
+		HashMap<Integer,List<String>> map=new HashMap<Integer,List<String>>();
+		List<String> data=new ArrayList<String>();
+		data.add("Kolkatta");
+		data.add("Mumbai");
+		data.add("Delhi");
+		data.add("Chennai");
+		map.put(1, data);
+		data.add("West Bengal");
+		data.add("Maharashtra");
+		data.add("New Delhi");
+		data.add("Tamilnaddu");
+		map.put(2, data);
+		for(Map.Entry<Integer, List<String>> m:map.entrySet()) {
+			System.out.println("Key -> " +m.getKey());
+			System.out.print("Value -> "+m.getValue());
+		}
+	}
+
+}
