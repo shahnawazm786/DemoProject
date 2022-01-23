dbb38205f8a218e5fe1feb4d0b4b3a555c5fcf5a
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-01-23 23:14:03 +0530

    Relative

--- src/test/java/selenium_4/RelativeExample.java
+++ src/test/java/selenium_4/RelativeExample.java
@@ -1,7 +1,10 @@
 package selenium_4;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
 
 public class RelativeExample {
 
@@ -11,8 +14,11 @@ public class RelativeExample {
 		System.setProperty("webdriver.chrome.driver", "src/test/resources/Drivers/chromedriver.exe");
 		driver=new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://www.google.com");
-		driver.quit();
+		driver.get("https://askomdch.com/account/");
+		By uname=RelativeLocator.with(By.tagName("input")).above(By.id("reg_email"));
+		driver.findElement(uname).sendKeys("shahnawaz");
+		
+		//driver.quit();
 		
 		
 	}
