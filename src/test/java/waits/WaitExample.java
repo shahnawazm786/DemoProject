0f12f3c110ab623c1d8710bef7b413a5f78fabf8
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-01-24 12:27:02 +0530

    wait

--- src/test/java/waits/WaitExample.java
+++ src/test/java/waits/WaitExample.java
@@ -1,11 +1,20 @@
 package waits;
 
+import org.openqa.selenium.WebDriver;
+
 public class WaitExample {
 
+	static WebDriver driver;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
-	
+	public static void startApps() {
+		
+		
+	}
+	public static void explicitWaitExample() {
+		
+	}
 
 }
