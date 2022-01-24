9caf69d9dd56bbecc8e4a7542559e6293bfd4f77
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-01-24 13:20:47 +0530

    screenshot

--- src/test/java/screenshot/ScreenshotExample.java
+++ src/test/java/screenshot/ScreenshotExample.java
@@ -1,7 +1,14 @@
 package screenshot;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class ScreenshotExample {
 	static WebDriver driver;
@@ -16,8 +23,10 @@ public class ScreenshotExample {
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com/");
 	}
-	public static void takeScreenshot() {
-		
+	public static void takeScreenshot() throws IOException {
+		File scrFile=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(scrFile, new File("src/test/resources/screenshot/image1.png"));
 	}
+	
 
 }
