13d1b89d0ce7b5649d01673ee3b5da1d782a0c28
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-01-24 13:04:45 +0530

    screenshot

--- src/test/java/screenshot/ScreenshotExample.java
+++ src/test/java/screenshot/ScreenshotExample.java
@@ -1,10 +1,23 @@
 package screenshot;
 
-public class ScreenshotExample {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class ScreenshotExample {
+	static WebDriver driver;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	public static void startApps() {
+		// set the property
+		System.setProperty("webdriver.chrome.driver", "src/test/resources/Drivers/chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+	}
+	public static void takeScreenshot() {
+		
 	}
 
 }
