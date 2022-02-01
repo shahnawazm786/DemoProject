fb0b5871953dd1821d51316cf6f3415754b55e98
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-02-01 10:39:09 +0530

    interview

--- src/main/java/interview/SwapNumberExample.java
+++ src/main/java/interview/SwapNumberExample.java
@@ -4,7 +4,19 @@ public class SwapNumberExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		usingThirdVariable();
+	}
+	static void usingThirdVariable() {
+		int num1=100;
+		int num2=200;
+		int temp;
+		System.out.println(" Value of Num1 is ->"+num1);
+		System.out.println(" Value of Num2 is ->"+num2);
+		temp=num1;
+		num1=num2;
+		num2=temp;
+		System.out.println(" Value of Num1 is ->"+num1);
+		System.out.println(" Value of Num2 is ->"+num2);
 	}
 
 }
