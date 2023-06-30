9b6e57ab06c331523d2dcfd446a5aebe65a60c3f
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-06-30 23:19:08 +0530

    Factory Classes

---
+++ src/test/java/awsome/Runner.java
@@ -0,0 +1,11 @@
+package awsome;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/awesome"
+)
+public class Runner {
+}
